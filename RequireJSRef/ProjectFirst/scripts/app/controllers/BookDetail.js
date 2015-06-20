c5c6fde093e13026a18a9d260a8bd94a06275a69
AlexBai1991 <baijinguo1991@163.com>
2015-06-21 01:15:08 +0800

    del unrelated files&folders

--- RequireJSRef/ProjectFirst/scripts/app/controllers/BookDetail.js
+++
@@ -1,9 +0,0 @@
-define(['mod'], function (app) {
-	app.controller('BookDetailController', ['$scope', function ($scope) {
-		$scope.book = {
-			id: '1',
-			name: 'AngularJS',
-			publishTime: '2014-2-19'
-		};
-	}]);
-});
